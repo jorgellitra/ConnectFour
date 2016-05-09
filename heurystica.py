8773a1df121a126442030961815e97cb10251db9
jorgellitra <jorge.llitra@gmail.com>
2016-05-09 12:13:36 +0100

    Casi perfecto

--- heurystica.py
+++ heurystica.py
@@ -3,8 +3,8 @@ from games import *
 def h1(state):
     bestX = 0
     bestO = 0
-    moveUses = legal_moves(state)
-    for x in moveUses:
+    legalMoves = legal_moves(state)
+    for x in legalMoves:
         nOV = k_in_row(state.board, x, 'O',(0,1))
         nOH = k_in_row(state.board, x, 'O',(1,0))
         nODD = k_in_row(state.board, x, 'O',(1,1))
@@ -20,9 +20,6 @@ def h1(state):
             bestO = totalO
         if totalX > bestX:
             bestX = totalX
-        if len(state.moves) == 0:
-            print('terminal')
-            return infinity
     return bestX - bestO
 
 def legal_moves(state):
@@ -31,58 +28,61 @@ def legal_moves(state):
             if y == 1 or (x, y-1) in state.board]
 
 def k_in_row (board, move, player,(delta_x,delta_y)):
-    x_inicial, y_inicial = move
     x, y = move
     n = 0
+    p = 1
     z = 0
-	
+
     while (board.get((x, y)) == None or board.get((x, y)) == player) and y < 8 and x < 7:
         if board.get((x, y)) == player:
             z += 1
             n += 500
         elif board.get((x, y)) == None:
-            n += 50 * funcionPos((x,y),(x_inicial,y_inicial))
+            n += 50
         x, y = x + delta_x, y + delta_y
-
+		
+    while (board.get((x, y)) == None or board.get((x, y)) == player) and y < 7 and x < 8:
+         if x == (x + delta_x):
+             if board.get((x, y)) == None:
+                 n += 50 * p
+                 p -= 0.10
+         elif y == (y + delta_y):
+             if board.get((x, y)) == player:
+                    z += 1
+                    n += 500
+             elif board.get((x, y)) == None:
+                 n += 50
+         elif y != (y + delta_y) and x != (x + delta_x):
+             if board.get((x, y)) == player:
+                 z += 1
+                 n += 500
+             elif board.get((x, y)) == None:
+                 n += 40 * p
+                 p -= 0.15
+         x, y = x + delta_x, y + delta_y
     x, y = move
-
+    p = 1
     while (board.get((x, y)) == None or board.get((x, y)) == player) and y > 0 and x > 0:
-        if board.get((x, y)) == player:
-            z += 1
-            n += 500
-        elif board.get((x, y)) == None:
-            n += 50 * funcionPos((x,y),(x_inicial,y_inicial))
-        x, y = x - delta_x, y - delta_y
-    if z >= 3:
+         if x == (x - delta_x):
+             if board.get((x, y)) == player:
+                 z += 1
+                 n += 500
+         elif y == (y - delta_y):
+             if board.get((x, y)) == player:
+                 z += 1
+                 n += 500
+             elif board.get((x, y)) == None:
+                 n += 50
+         elif y != (y + delta_y) and x != (x + delta_x):
+             if board.get((x, y)) == player:
+                 z += 1
+                 n += 500
+             elif board.get((x, y)) == None:
+                 n += 40 * p
+                 p -= 0.15
+         x, y = x - delta_x, y - delta_y
+    if z == 3:
         n += 5000
+    if z >= 4:
+        return infinity
     return n
-
-def funcionPos(move, inicial):
-    x ,y = move
-    x_inicial, y_inicial = inicial
-    if (x-x_inicial == 0 and y-y_inicial == 1):
-        return 0.7
-    elif (x-x_inicial == 0 and y-y_inicial == 2):
-        return 0.5
-    elif (x-x_inicial == 0 and y-y_inicial == 3):
-        return 0.3
-    elif (x-x_inicial == 0 and y-y_inicial == -1):
-        return 0.7
-    elif (x-x_inicial == 0 and y-y_inicial == -2):
-        return 0.5
-    elif (x-x_inicial == 0 and y-y_inicial == -3):
-        return 0.3
-    elif (x-x_inicial == 1 and y-y_inicial == 1):
-        return 0.6
-    elif (x-x_inicial == 2 and y-y_inicial == 2):
-        return 0.3
-    elif (x-x_inicial == 3 and y-y_inicial == 3):
-        return 0.1
-    elif (x-x_inicial == -1 and y-y_inicial == -1):
-        return 0.6
-    elif (x-x_inicial == -2 and y-y_inicial == -2):
-        return 0.3
-    elif (x-x_inicial == -3 and y-y_inicial == -3):
-        return 0.1
-    else:
-        return 1
